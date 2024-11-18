4cf187a1b61f6f6341cbc44e07428ad449e3d87d
Khadiii1811 <toankhoa86443@gmail.com>
2024-11-18 07:45:29 +0700

    khoa

--- routes/article_list.route.js
+++ routes/article_list.route.js
@@ -1,10 +1,12 @@
 import express from 'express';
-
-
+import articleService from '../services/reader/article_list.service.js'; 
 const router = express.Router();
-router.get('/', function(req, res) {
+router.get('/', async function(req, res) {
+    const articles = await articleService.findAll();
+
     res.render('vwArticle/list', {
         layout: false,
+        articles: articles 
     });
 });
 
